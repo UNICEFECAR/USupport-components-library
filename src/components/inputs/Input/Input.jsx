84cfa65dee2205b8fddd46d84bec88129082b47a
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-10-01 11:59:26 +0300

    Create: Input and InputPassword components

---
+++ src/components/inputs/Input/Input.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./input.scss";
+
+/**
+ * Input
+ *
+ * Base Text input component
+ *
+ * @return {jsx}
+ */
+export const Input = ({
+  type,
+  label,
+  disabled,
+  errorMessage,
+  children,
+  ...rest
+}) => {
+  return (
+    <>
+      <div
+        className={[
+          "input-container",
+          disabled && "disabled",
+          errorMessage && "error",
+        ].join(" ")}
+      >
+        {label ? <p className="label">{label}</p> : null}
+        <div className="input-wrapper">
+          <input type={type} disabled={disabled} className="input" {...rest} />
+          {children ? children : null}
+        </div>
+      </div>
+      {errorMessage ? <p className="error-message">{errorMessage}</p> : null}
+    </>
+  );
+};
+
+Input.propTypes = {
+  /**
+   * Input type
+   *@default "text"
+   **/
+  type: PropTypes.oneOf(["text"]),
+
+  /**
+   * Input label
+   **/
+  label: PropTypes.string,
+
+  /**
+   * Input disabled
+   * @default false
+   **/
+  disabled: PropTypes.bool,
+};
+
+Input.defaultProps = {
+  type: "text",
+  disabled: false,
+};
---
+++ src/components/inputs/Input/Input.stories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Input } from "./Input";
+
+export default {
+  title: "Components Library/inputs/Input",
+  component: Input,
+  argTypes: {},
+};
+
+const Template = (args) => <Input placeholder={"Placeholder"} {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "Input",
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+  label: "Input",
+  errorMessage: "Error message",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: "Input",
+  disabled: true,
+};
