5a12c194f3fc1c27895495a8a668cd5e1a9353d5
Sebastian Galustyan <sebastian@7digit.io>
2022-10-02 22:48:29 +0100

    Create: new Grid component

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export * from "./components/icons";
 export * from "./components/buttons";
 export * from "./components/cards";
 export * from "./components/inputs";
+export * from "./components/grids";
