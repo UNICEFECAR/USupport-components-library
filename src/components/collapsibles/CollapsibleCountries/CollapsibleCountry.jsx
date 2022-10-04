f50b4df0d5830e0edff1402342f1c3d72e50013c
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-10-05 01:46:46 +0300

    Create: CollapsibleCountry component

---
+++ src/components/collapsibles/CollapsibleCountries/CollapsibleCountry.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./collapsiblecountry.scss";
+import { Collapsible } from "../Collapsible/Collapsible";
+import { IconFlag } from "../../icons/IconFlag/IconFlag";
+
+/**
+ * ComponentNew
+ *
+ * Component
+ *
+ * @return {jsx}
+ */
+export const CollapsibleCountry = ({ country, handleOnClickLanguage }) => {
+  const renderCountryHeading = () => {
+    return (
+      <div className="country-details">
+        <div className="flag-container">
+          <IconFlag flagName={country.flagName} />
+        </div>
+        <p className="text">{country.name}</p>
+      </div>
+    );
+  };
+
+  const renderLanguages = () => {
+    return (
+      <div className="languages">
+        {country.languages.map((language) => {
+          return (
+            <p className="text" onClick={handleOnClickLanguage}>
+              {language}
+            </p>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <div className="country">
+      <Collapsible
+        heading={renderCountryHeading(country)}
+        collapsibleContent={renderLanguages(country)}
+        iconColor={"#20809E"}
+        iconSize="md"
+      />
+    </div>
+  );
+};
+
+CollapsibleCountry.propTypes = {
+  /**
+   * Country object
+   **/
+  country: PropTypes.object,
+
+  /**
+   * Function to handle click on language
+   **/
+  handleOnClickLanguage: PropTypes.func,
+};
+
+CollapsibleCountry.defaultProps = {
+  handleOnClickLanguage: () => {},
+};
