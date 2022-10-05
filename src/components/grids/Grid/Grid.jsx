5ef8e37915f888b082b5a261b18634fe812f6b52
Georgi 7DIGIT <georgi@7digit.io>
2022-10-05 04:34:13 +0300

    Refactor: the grid and grid item components to use a div instead of a box

--- src/components/grids/Grid/Grid.jsx
+++ src/components/grids/Grid/Grid.jsx
@@ -14,23 +14,24 @@ import "./grid.scss";
  */
 export const Grid = ({ classes, style, children, ...props }) => {
   return (
-    <Box classes={`grid ${classNames(classes)}`} style={style} {...props}>
+    <div className={`grid ${classNames(classes)}`} style={style} {...props}>
       {children}
-    </Box>
+    </div>
   );
 };
 
 Grid.propTypes = {
   /**
    * Additional classes to be added to the grid
-   **/
+   */
   classes: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.arrayOf(PropTypes.string),
   ]),
+
   /**
    * Additional styles to be added to the Grid
-   **/
+   */
   style: PropTypes.object,
 };
 
--- src/components/grids/GridItem/GridItem.jsx
+++ src/components/grids/GridItem/GridItem.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box } from "../../boxes/Box/Box";
 import classNames from "classnames";
 
 import "./griditem.scss";
@@ -14,35 +13,37 @@ import "./griditem.scss";
  */
 export const GridItem = ({ xs, md, lg, children, classes, ...props }) => {
   return (
-    <Box
-      classes={[
+    <div
+      className={[
         `grid-item xs-${xs} md-${md} lg-${lg}`,
         classNames(classes),
       ].join(" ")}
       {...props}
     >
       {children}
-    </Box>
+    </div>
   );
 };
 
 GridItem.propTypes = {
   /**
    * Number of columns to span on extra small screens
-   **/
+   */
   xs: PropTypes.oneOf([1, 2, 3, 4]),
 
   /**
    * Number of columns to span on medium screens
-   **/
+   */
   md: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8]),
 
   /**
    * Number of columns to span on large screens
-   **/
+   */
   xl: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
 
-  // Add defaultProps here
+  /**
+   * Additional classes to be added to the grid item
+   */
   classes: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.arrayOf(PropTypes.string),
