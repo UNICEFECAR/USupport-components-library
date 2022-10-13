6046a73c5009bb083ba18839dfa3c07bd542619d
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-10-13 13:56:42 +0300

    Create: date.js util

---
+++ src/utils/date.js
@@ -0,0 +1,23 @@
+function getDayOfTheWeek(date) {
+  const day = date.getDay();
+  switch (day) {
+    case 0:
+      return "Sun";
+    case 1:
+      return "Mon";
+    case 2:
+      return "Tue";
+    case 3:
+      return "Wed";
+    case 4:
+      return "Thu";
+    case 5:
+      return "Fri";
+    case 6:
+      return "Sat";
+    default:
+      return "Unknown";
+  }
+}
+
+export { getDayOfTheWeek };
