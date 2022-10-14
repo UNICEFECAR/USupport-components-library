8162f07ad7f941328f31bdd70622925570bfd39e
Georgi 7DIGIT <georgi@7digit.io>
2022-10-14 18:35:52 +0300

    Fix: text colors and wrong proptypes in SingleDay compnent

--- src/components/calendar/SingleDay/SingleDay.jsx
+++ src/components/calendar/SingleDay/SingleDay.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes, { number } from "prop-types";
+import PropTypes from "prop-types";
 
 import "./single-day.scss";
 import { getDayOfTheWeek } from "../../../utils";
@@ -12,6 +12,7 @@ import { getDayOfTheWeek } from "../../../utils";
  * @return {jsx}
  */
 export const SingleDay = ({ date, numberOfConsultations }) => {
+  // TODO: Do we take the Day string from the props or we translate here
   const isToday = new Date().toDateString() === date.toDateString();
 
   return (
@@ -22,11 +23,20 @@ export const SingleDay = ({ date, numberOfConsultations }) => {
         isToday && "single-day__today",
       ].join(" ")}
     >
-      <p className="text">{`${isToday ? "Today, " : ""}${getDayOfTheWeek(
-        date
-      )}`}</p>
-      <h4>{date.getDate()}</h4>
-      <p className="small-text single-day__consultation-text">
+      <p
+        className={["text", isToday ? "single-day__day-name--today" : ""].join(
+          " "
+        )}
+      >{`${isToday ? "Today, " : ""}${getDayOfTheWeek(date)}`}</p>
+      <h4 className={isToday ? "single-day__date--today" : ""}>
+        {date.getDate()}
+      </h4>
+      <p
+        className={[
+          "small-text single-day__consultation-text",
+          isToday ? "single-day__consultation-text--today" : "",
+        ].join(" ")}
+      >
         {numberOfConsultations} consultations
       </p>
     </div>
@@ -37,7 +47,7 @@ SingleDay.propTypes = {
   /**
    * Date
    **/
-  date: PropTypes.string,
+  date: PropTypes.Date,
 
   /**
    * Number of consultations
@@ -46,6 +56,6 @@ SingleDay.propTypes = {
 };
 
 SingleDay.defaultProps = {
-  date: "",
+  date: new Date(),
   numberOfConsultations: 0,
 };
