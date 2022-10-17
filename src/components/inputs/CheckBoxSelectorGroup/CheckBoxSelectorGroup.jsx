444e8a0cef9ee55906cc56dd303ea1a049a38bd6
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-10-17 14:03:12 +0300

    Refactor: CheckBoxGroup component to take classes

---
+++ src/components/inputs/CheckBoxSelectorGroup/CheckBoxSelectorGroup.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './check-box-selector-group.scss';
+
+/**
+ * CheckBoxSelectorGroup
+ *
+ * CheckBoxSelectorGroup component
+ *
+ * @return {jsx}
+ */
+export const CheckBoxSelectorGroup = ({/* Add props here */}) => {
+  return (
+    <h1>
+      CheckBoxSelectorGroup Component
+    </h1>
+  );
+};
+
+CheckBoxSelectorGroup.propTypes = {
+  // Add propTypes here
+};
+
+CheckBoxSelectorGroup.defaultProps = {
+  // Add defaultProps here
+};
