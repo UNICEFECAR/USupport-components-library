8394f14d08230e0b53df2ff88f001fde6456d2de
Georgi 7DIGIT <georgi@7digit.io>
2022-10-29 16:39:37 +0300

    Fix: styling of labels in CardMedia and fix the name of the story

--- src/components/cards/CardMedia/CardMedia.stories.jsx
+++ src/components/cards/CardMedia/CardMedia.stories.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { CardMedia } from "./CardMedia";
 
 export default {
-  title: "Components Library/cards/CarMedia",
+  title: "Components Library/cards/CardMedia",
   component: CardMedia,
   argTypes: {},
 };
@@ -12,7 +12,16 @@ const data = {
   description:
     "U support Me is the first platform that provides you with personalized care for your mental health and personal development and provides you...",
   title: "How to stop negative thinking? da da d sa d as da. aa sd",
-  labels: [{ name: "Depresion" }, { name: "Anxiety" }],
+  labels: [
+    { name: "Depresion" },
+    { name: "Anxiety" },
+    { name: "Talking" },
+    { name: "Breathing" },
+    { name: "Lorem" },
+    {
+      name: "Self-imporvement and some more long words to see if it wraps correctly and some more words",
+    },
+  ],
   creator: { name: "Joe Doe" },
   readingTime: 5,
 };
