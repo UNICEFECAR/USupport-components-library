1a6d8765821c5c2543df73d5d24076dbdd2d9fb0
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-10-31 20:35:06 +0200

    Fix: classes names in ConsultationDashboard

--- src/components/cards/ConsultationDashboard/ConsultationDashboard.jsx
+++ src/components/cards/ConsultationDashboard/ConsultationDashboard.jsx
@@ -27,15 +27,18 @@ export const ConsultationDashboard = ({
   scheduleButtonText,
 }) => {
   return (
-    <Box shadow={1} classes={["consultation", classNames(classes)].join(" ")}>
+    <Box
+      shadow={1}
+      classes={["consultation-dashboard", classNames(classes)].join(" ")}
+    >
       {specialistName && consultationDate ? (
-        <div className="consultation__content">
+        <div className="consultation-dashboard__content">
           {isLive ? (
             <p className="small-text now-text">{liveText}</p>
           ) : (
             <p className="small-text">{consultationDate}</p>
           )}
-          <div className="consultation__content__specialist-container">
+          <div className="consultation-dashboard__content__specialist-container">
             <img src={avatar} className="specialist-image" />
             <p className="text">{specialistName}</p>
           </div>
@@ -44,7 +47,7 @@ export const ConsultationDashboard = ({
               label={joinButtonText}
               color="purple"
               size="sm"
-              classes="consultation__button"
+              classes="consultation-dashboard__button"
             />
           ) : (
             <Button
@@ -56,7 +59,7 @@ export const ConsultationDashboard = ({
           )}
         </div>
       ) : (
-        <div className="consultation__no-consultation">
+        <div className="consultation-dashboard__no-consultation">
           <p className="small-text no-booking-text">{noConsultationsText}</p>
           <Button size="sm" label={scheduleButtonText} color="purple" />
         </div>
