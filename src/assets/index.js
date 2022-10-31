61fdb3a49af6adef16276073400d405e4edcf1f5
Vasilen Tsvetkov <vasilen@7digit.io>
2022-10-31 16:09:02 +0000

    Refactor: general restructuring

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,5 +11,7 @@ export { default as logoHorizontalSvg } from "./logo-horizontal.svg";
 export { default as logoSquarePng } from "./logo-square.png";
 export { default as logoSquareSvg } from "./logo-square.svg";
 export { default as specialistPlaceholder } from "./SpecialistPlaceholderImage.png";
-export { default as mascotConfusedBlue } from "./mascot-confused-blue.png";
+export { default as mascotCalmBlue } from "./mascot-calm-blue.png";
 export { default as mascotHappyBlue } from "./mascot-happy-blue.png";
+export { default as mascotHappyBlueClipped } from "./mascot-happy-blue-clipped.png";
+export { default as mascotConfusedBlue } from "./mascot-confused-blue.png";
