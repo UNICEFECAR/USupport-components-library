abf1ff7f0df678e875c2ddd12676c107b742d374
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-11-03 15:34:08 +0200

    Refactor: CheckBox add onChange to the input

--- src/components/inputs/CheckBox/CheckBox.jsx
+++ src/components/inputs/CheckBox/CheckBox.jsx
@@ -29,6 +29,7 @@ export const CheckBox = ({
         checked={isChecked}
         className={[isChecked ? "checked" : ""]}
         disabled={disabled}
+        onChange={() => {}}
         {...props}
       />
       {label ? <p className="text label">{label}</p> : null}
