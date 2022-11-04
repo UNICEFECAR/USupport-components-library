163dd3ad9bed1c7330781e6a08eb3adb999944ca
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-11-04 15:33:15 +0200

    Refactor: date to be in correct format in Consultation component

--- src/components/cards/Consultation/Consultation.jsx
+++ src/components/cards/Consultation/Consultation.jsx
@@ -36,7 +36,11 @@ export const Consultation = ({
   // TODO: Figure out a way to translate the days of the week
   // Idea: Create a reuseable hook that takes a string e.g. "mon" and returns the day translated
   const dateText = startDate
-    ? `${getDayOfTheWeek(startDate)}, ${startDate.getDate()}.${
+    ? `${getDayOfTheWeek(startDate)}, ${
+        startDate.getDate() < 10
+          ? `0${startDate.getDate()}`
+          : startDate.getDate()
+      }.${
         startDate.getMonth() < 10
           ? `0${startDate.getMonth()}`
           : startDate.getMonth()
