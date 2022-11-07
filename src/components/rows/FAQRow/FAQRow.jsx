d5067359180ad244a793dc04b6dc2af77611843e
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-11-07 21:56:06 +0200

    Create: FAQRow component

---
+++ src/components/rows/FAQRow/FAQRow.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { CheckBox } from "../../inputs/CheckBox/CheckBox";
+
+import "./faq-row.scss";
+
+/**
+ * FAQRow
+ *
+ * FAQRow component
+ *
+ * @return {jsx}
+ */
+export const FAQRow = ({ selected, setSelected, question, answer }) => {
+  return (
+    <div className="faq-row">
+      <CheckBox isChecked={selected} setIsChecked={setSelected} />
+      <div className="faq-row__text-container">
+        <h4>{question}</h4>
+        <p className="text">{answer}</p>
+      </div>
+    </div>
+  );
+};
+
+FAQRow.propTypes = {
+  /**
+   * selected
+   * @type {boolean}
+   * @default false
+   */
+  selected: PropTypes.bool,
+
+  /**
+   * Function to set selected
+   * @type {function}
+   * @default () => {}
+   */
+  setSelected: PropTypes.func,
+
+  /**
+   * Question to be displayed
+   * */
+  question: PropTypes.string,
+
+  /**
+   * Answer to be displayed
+   * */
+  answer: PropTypes.string,
+};
+
+FAQRow.defaultProps = {
+  selected: false,
+  setSelected: () => {},
+};
