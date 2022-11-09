07a92431abc34fc2cea9f2a2e2ed5b68076d325f
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-11-09 20:23:23 +0200

    Fix: Date format in SingleDay component

--- src/components/calendar/SingleDay/SingleDay.jsx
+++ src/components/calendar/SingleDay/SingleDay.jsx
@@ -23,8 +23,10 @@ export const SingleDay = ({
   const isToday = new Date().toDateString() === date.toDateString();
 
   const dateAsString = `${
-    date.getDate() > 9 ? date.getDate() : 0 + date.getDate()
-  }.${date.getMonth() + 1 > 9 ? date.getMonth() + 1 : 0 + date.getMonth() + 1}`;
+    date.getDate() > 9 ? date.getDate() : "0" + date.getDate()
+  }.${
+    date.getMonth() + 1 > 9 ? date.getMonth() + 1 : "0" + date.getMonth() + 1
+  }`;
 
   return (
     <div
