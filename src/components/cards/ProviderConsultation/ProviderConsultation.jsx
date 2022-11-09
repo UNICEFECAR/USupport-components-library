a40235112c2c9124cd94c2e9888c15296972ec4d
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2022-11-09 08:47:11 +0200

    Refactor: ProviderConsultation component

--- src/components/cards/ProviderConsultation/ProviderConsultation.jsx
+++ src/components/cards/ProviderConsultation/ProviderConsultation.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { Box } from "../../boxes/Box/Box";
+import { specialistPlaceholder } from "../../../assets";
+import { Button } from "../../buttons/Button/Button";
+import { ButtonOnlyIcon } from "../../buttons/ButtonOnlyIcon/ButtonOnlyIcon";
 import { Icon } from "../../icons/Icon";
 
 import "./provider-consultation.scss";
-
-import { specialistPlaceholder } from "../../../assets";
-import { Button } from "../../buttons/Button/Button";
 
 /**
  * ProviderConsultation
@@ -29,55 +29,130 @@ export const ProviderConsultation = ({
   isCompleted,
 }) => {
   // TODO: check if the consultation is live by comparing the current hour with the consultation date
-  const isLive = false;
-  const isUpcoming = true;
+  const isLive = true;
+  const isUpcoming = false;
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const renderOptions = () => {
+    let menuOptions = [
+      {
+        iconName: "person",
+        text: "View personal profile",
+        onClick: () => {
+          console.log("View personal profile");
+        },
+      },
+    ];
+
+    if (isUpcoming || isLive) {
+      menuOptions.push({
+        iconName: "close-x",
+        text: "Cancel consultation",
+        onClick: () => console.log("Cancel consultation"),
+      });
+    }
+
+    if (isCompleted) {
+      menuOptions.push({
+        iconName: "document",
+        text: "Check details",
+        onClick: () => console.log("Check details"),
+      });
+    }
+
+    return menuOptions.map((option, index) => {
+      return (
+        <div
+          className="provider-consultation-container__menu__option"
+          onClick={option.onClick}
+          key={index}
+        >
+          <Icon
+            name={option.iconName}
+            color={"#373737"}
+            classes="provider-consultation-container__menu__option__icon"
+          />
+          <p className="small-text">{option.text}</p>
+        </div>
+      );
+    });
+  };
 
   return (
-    <Box
-      borderSize="xs"
-      classes={["provider-consultation", classNames(classes)]}
+    <div
+      className={["provider-consultation-container", classNames(classes)].join(
+        " "
+      )}
     >
-      <div className="provider-consultation__information-container">
-        <img
-          className="provider-consultation__user-image"
-          src={image}
-          alt="user-image"
-        />
-        <div>
-          <p className="small-text">{name}</p>
-          <p className="text">{period}</p>
-        </div>
-        <div className="provider-consultation__icon-container">
-          <Icon name="three-dots-vertical" />
-        </div>
-      </div>
-      <div className="provider-consultation__action-container">
-        {isCompleted && (
-          <p className="small-text provider-consultation__completed-text">
-            {consultationEndedText}
-          </p>
-        )}
-
-        {isLive && (
-          <>
-            <p className="small-text provider-consultation__live-text">
-              {isLiveText}
-            </p>
-            <Button color="purple" size="md" label={isLiveButtonText} />
-          </>
-        )}
-
-        {isUpcoming && (
-          <Button
-            classes="provider-consultation__change-button"
-            color="purple"
-            size="md"
-            label={upcomingButtonText}
-            type="secondary"
+      <Box
+        borderSize="xs"
+        classes={[
+          "provider-consultation",
+          isLive && "provider-consultation-live",
+        ]}
+      >
+        <div className="provider-consultation__information-container">
+          <img
+            className="provider-consultation__user-image"
+            src={image}
+            alt="user-image"
           />
-        )}
-      </div>
-    </Box>
+          <div>
+            <p className="small-text">{name}</p>
+            <p className="text">{period}</p>
+          </div>
+          <div className="provider-consultation__icon-container">
+            <ButtonOnlyIcon
+              iconName="three-dots-vertical"
+              iconSize="md"
+              iconColor={isLive ? "#9749FA" : "#156F8C"}
+              onClick={handleToggleMenu}
+            />
+          </div>
+        </div>
+        <div className="provider-consultation__action-container">
+          {isCompleted && (
+            <p className="small-text provider-consultation__completed-text">
+              {consultationEndedText}
+            </p>
+          )}
+
+          {isLive && (
+            <>
+              <p className="small-text provider-consultation__live-text">
+                {isLiveText}
+              </p>
+              <Button color="purple" size="md" label={isLiveButtonText} />
+            </>
+          )}
+
+          {isUpcoming && (
+            <Button
+              classes="provider-consultation__change-button"
+              color="purple"
+              size="md"
+              label={upcomingButtonText}
+              type="secondary"
+            />
+          )}
+        </div>
+      </Box>
+      {isMenuOpen && (
+        <div
+          className={[
+            "provider-consultation-container__menu",
+            isLive && "provider-consultation-container__menu-live",
+          ].join(" ")}
+        >
+          {renderOptions()}
+        </div>
+      )}
+    </div>
   );
 };
 
