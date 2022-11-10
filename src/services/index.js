cd28aa3bb8918e1585b317f122358e3360f8504e
Georgi 7DIGIT <georgi@7digit.io>
2022-11-10 19:31:35 +0200

    Add: client service export

--- src/services/index.js
+++ src/services/index.js
@@ -4,4 +4,5 @@ export { default as httpSvc } from "./http.js";
 export { default as userSvc } from "./user.js";
 export { default as languageSvc } from "./language.js";
 export { default as countrySvc } from "./country.js";
+export { default as clientSvc } from "./client.js";
 export * from "./log.js";
