aaa546a45f2238b77dfe13b766bcd6fbc724ddc9
Georgi 7DIGIT <georgi@7digit.io>
2022-11-12 23:56:52 +0200

    Refactor: the get notification preferences hook to transform the properties of the data received from the API

--- src/hooks/useGetNotificationPreferences.js
+++ src/hooks/useGetNotificationPreferences.js
@@ -1,48 +1,31 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { userSvc } from "../services";
 
 /**
  * Reuseable hook to get and transform the client data in a desired format
  */
 export default function useGetNotificationPreferences() {
-  const queryClient = useQueryClient();
-  const [notificationPreferences, setNotificationPreferences] = useState();
   const fetchNotificationsData = async () => {
-    const res = await userSvc.getNotificationPreferences();
-    return res.data;
+    const { data } = await userSvc.getNotificationPreferences();
+    let transformedData = {
+      consultationReminder: data.consultation_reminder,
+      consultationReminderMin: data.consultation_reminder_min,
+      email: data.email,
+      inPlatform: data.in_platform,
+      push: data.push,
+    };
+    return transformedData;
   };
 
   const notificationsPreferencesQuery = useQuery(
     ["notification-preferences"],
     fetchNotificationsData,
     {
-      onSuccess: (data) => {
-        const dataCopy = JSON.parse(JSON.stringify(data));
-        setNotificationPreferences({ ...dataCopy });
-      },
-      onError: (err) => console.log(err, "err"),
       notifyOnChangeProps: ["data"],
     }
   );
 
-  const update = (data) => {
-    // queryClient.setQueryData(["notification-preferences"], data);
-  };
-
-  //   useEffect(() => {
-  //     const queryData = notificationsPreferencesQuery.data;
-  //     const stateData = notificationPreferences;
-  //     console.log("data changed");
-  //     if (queryData && stateData) {
-  //       console.log("difference");
-  //       if (JSON.stringify(queryData) !== JSON.stringify(stateData)) {
-  //         setNotificationPreferences(queryData);
-  //       }
-  //     }
-  //   }, [notificationsPreferencesQuery.data]);
-
-  return [notificationsPreferencesQuery, update];
+  return [notificationsPreferencesQuery];
 }
 
 export { useGetNotificationPreferences };
