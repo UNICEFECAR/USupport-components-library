6bf5aaf059fdf01d1d9de338fb0ebca3b8f09535
Georgi 7DIGIT <georgi@7digit.io>
2022-11-19 01:10:53 +0200

    Add: functions to the provider service

--- src/services/provider.js
+++ src/services/provider.js
@@ -73,6 +73,56 @@ async function addTemplateAvailability(data) {
   return response;
 }
 
+async function getAllProviders() {
+  const response = await http.get(`${API_ENDPOINT}/all`);
+  return response;
+}
+
+async function getProviderById(id) {
+  const response = await http.get(`${API_ENDPOINT}/by-id?providerId=${id}`);
+  return response;
+}
+
+/**
+ *
+ * @param {number} startDate the timestamp of the start date of the week
+ * @param {number} day the timestamp of the desired day
+ * @param {String} providerId the id of the provider
+ * @returns {Promise} resolving to an array with all the slots for the day
+ */
+async function getAvailableSlotsForSingleDay(startDate, day, providerId) {
+  const response = await http.get(
+    `${API_ENDPOINT}/availability/single-day?providerId=${providerId}&startDate=${startDate}&day=${day}`
+  );
+  return response;
+}
+
+/**
+ *
+ * @param {String} providerId the id of the provider
+ * @param {number} slotTimestamp the timestamp of the slot
+ * @returns {Promise} resolving to and object with the "consultation_id"
+ */
+async function blockSlot(providerId, slotTimestamp) {
+  const response = await http.post(`${API_ENDPOINT}/consultation/block`, {
+    providerId,
+    time: JSON.stringify(slotTimestamp / 1000),
+  });
+  return response;
+}
+
+/**
+ *
+ * @param {String} consultationId the id of the consultation
+ * @returns {Promise}
+ */
+async function scheduleConsultation(consultationId) {
+  const response = await http.put(`${API_ENDPOINT}/consultation/schedule`, {
+    consultationId,
+  });
+  return response;
+}
+
 const exportedFunctions = {
   getProviderData,
   updateProviderData,
@@ -82,5 +132,10 @@ const exportedFunctions = {
   addAvailableSlot,
   removeAvailableSlot,
   addTemplateAvailability,
+  getAllProviders,
+  getProviderById,
+  getAvailableSlotsForSingleDay,
+  blockSlot,
+  scheduleConsultation,
 };
 export default exportedFunctions;
