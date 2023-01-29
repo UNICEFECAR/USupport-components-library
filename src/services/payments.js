13cf3f867508dd619c5a6a01bc5091d538c43ae6
Sebastian Galustyan <sebastian@7digit.io>
2023-01-29 20:13:29 +0000

    Refactor: getPaymentHistory service function

--- src/services/payments.js
+++ src/services/payments.js
@@ -23,8 +23,13 @@ async function createPaymentIntent(consultationId) {
  *
  * @returns {Promise} - Promise object represents the response from the server containing the client secret
  */
-async function getPaymentHistory() {
-  const response = await http.get(`${API_ENDPOINT}/one-time/history`);
+async function getPaymentHistory({ limit, startingAfterPaymentIntentId }) {
+  const response = await http.get(`${API_ENDPOINT}/one-time/history`, {
+    params: {
+      limit: limit,
+      start_after_payment_intent_id: startingAfterPaymentIntentId,
+    },
+  });
 
   return response;
 }
