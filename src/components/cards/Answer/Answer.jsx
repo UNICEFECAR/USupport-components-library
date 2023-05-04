e04a4a191ff316d93fe8d9bd968976a384193bbc
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-05-04 20:16:51 +0300

    Fix: Answer component styling

--- src/components/cards/Answer/Answer.jsx
+++ src/components/cards/Answer/Answer.jsx
@@ -144,22 +144,22 @@ export const Answer = ({
               classes="answer__read-more-button"
               onClick={() => handleReadMore(question)}
             />
-            {renderIn === "provider" ||
-              (renderIn === "country-admin" && (
-                <div className="answer__answered-by-container">
-                  <p className="text">{t("answer_by")}</p>
-                  <Avatar
-                    image={AMAZON_S3_BUCKET + "/" + providerInfo.image}
-                    alt="Specialist avatar"
-                    size="xs"
-                    classes="answer__answered-by-container__avatar"
-                  />
-                  <p className="text">
-                    {providerInfo.name} {providerInfo.surname}
-                  </p>
-                </div>
-              ))}
           </div>
+          {renderIn === "provider" ||
+            (renderIn === "country-admin" && (
+              <div className="answer__answered-by-container">
+                <p className="text">{t("answer_by")}</p>
+                <Avatar
+                  image={AMAZON_S3_BUCKET + "/" + providerInfo.image}
+                  alt="Specialist avatar"
+                  size="xs"
+                  classes="answer__answered-by-container__avatar"
+                />
+                <p className="text">
+                  {providerInfo.name} {providerInfo.surname}
+                </p>
+              </div>
+            ))}
           {renderIn === "client" ? (
             <div className="answer__bottom-container">
               <div className="answer__answered-by-container">
