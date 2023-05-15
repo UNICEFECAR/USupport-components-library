46c564216cda77d59d62e60d57846dec28a44755
Georgi 7DIGIT <georgi@7digit.io>
2023-05-15 20:16:59 +0300

    Fix: show remove button in the InputGroup on the first option if there are more than 1 options

--- src/components/inputs/InputGroup/InputGroup.jsx
+++ src/components/inputs/InputGroup/InputGroup.jsx
@@ -90,7 +90,7 @@ export const InputGroup = ({
                   label={`${label} ${index + 1}${index === 0 ? " *" : ""}`}
                   placeholder={label}
                 />
-                {index !== 0 && (
+                {(options.length > 1 ? true : index !== 0) && (
                   <ButtonWithIcon
                     color="red"
                     classes="input-group__inputs-list__single__remove-button"
