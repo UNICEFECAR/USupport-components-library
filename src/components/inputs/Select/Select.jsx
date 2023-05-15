30b00e1988ffd8de1755192efcf0864abcc65fbc
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-05-15 11:30:39 +0300

    Create: Select component with react-select library

---
+++ src/components/inputs/Select/Select.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import ReactSelect from "react-select";
+import makeAnimated from "react-select/animated";
+
+import { Error } from "../../errors";
+
+import "./select.scss";
+
+/**
+ * Select
+ *
+ * inputs
+ *
+ * @return {jsx}
+ */
+export const Select = ({
+  options,
+  placeholder,
+  errorMessage,
+  handleChange,
+  label,
+  classes,
+  ...rest
+}) => {
+  useEffect(() => {
+    setSelectedOptions(options.filter((option) => option.selected));
+  }, []);
+
+  const [selectedOptions, setSelectedOptions] = useState(null);
+
+  const handleSelect = (data) => {
+    setSelectedOptions(data);
+    handleChange(
+      options.map((option) => {
+        if (data.includes(option)) {
+          option.selected = true;
+        }
+        return option;
+      })
+    );
+  };
+
+  return (
+    <div className={classes} style={{ marginTop: "24px" }}>
+      {label ? <p className="text select-container__label">{label}</p> : null}
+      <ReactSelect
+        placeholder={<p className="select__placeholder">{placeholder}</p>}
+        options={options}
+        value={selectedOptions}
+        onChange={handleSelect}
+        className="select-container"
+        classNamePrefix={"select"}
+        closeMenuOnSelect={false}
+        isMulti
+        {...rest}
+      />
+      {errorMessage && <Error message={errorMessage} />}
+    </div>
+  );
+};
+
+Select.propTypes = {
+  // Add propTypes here
+};
+
+Select.defaultProps = {
+  // Add defaultProps here
+};
