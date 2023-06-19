eaca28c6b1e92d5548e38bbb204fa873a81987e7
Georgi 7DIGIT <georgi@7digit.io>
2023-06-19 22:04:57 +0300

    Fix: remove the manual setting of the "Origin" header

--- src/services/http.js
+++ src/services/http.js
@@ -9,8 +9,6 @@ const VITE_WEBSITE_URL = import.meta.env.VITE_WEBSITE_URL;
 axios.interceptors.request.use((config) => {
   config.headers["x-country-alpha-2"] = localStorage.getItem("country") || "";
   config.headers["x-language-alpha-2"] = localStorage.getItem("language") || "";
-  config.headers["Origin"] =
-    VITE_WEBSITE_URL || "https://staging.usupport.online";
 
   const requestURI = axios.getUri(config) || "VITE CMS API URL";
 
