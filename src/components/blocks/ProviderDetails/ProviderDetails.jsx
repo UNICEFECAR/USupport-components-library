ef63fce77b87b3615289acf2f9421d17a320789b
Georgi 7DIGIT <georgi@7digit.io>
2024-09-02 16:02:19 +0300

    Add: field to display organizations in ProviderDetails

--- src/components/blocks/ProviderDetails/ProviderDetails.jsx
+++ src/components/blocks/ProviderDetails/ProviderDetails.jsx
@@ -226,6 +226,14 @@ export const ProviderDetails = ({
                 {provider.totalConsultations} {t("consultations")}
               </p>
             </div>
+            <div className="provider-details__information-container">
+              <p className="paragraph provider-details__information-container__heading">
+                {t("organization_label")}
+              </p>
+              <p className="paragraph provider-details__information-container__text">
+                {provider.organizations?.map((org) => org.name).join(", ")}
+              </p>
+            </div>
           </GridItem>
         </Grid>
       </GridItem>
