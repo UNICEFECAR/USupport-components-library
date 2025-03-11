3a1e9e4449b85532336cde1687f02547fd37eff6
Georgi 7DIGIT <georgi@7digit.io>
2025-03-11 12:41:54 +0200

    Fix: glitch when changing country and navigating between pages

--- src/components/navbars/Navbar/Navbar.jsx
+++ src/components/navbars/Navbar/Navbar.jsx
@@ -24,6 +24,12 @@ const kazakhstanCountry = {
   iconName: "KZ",
 };
 
+const polandCountry = {
+  value: "PL",
+  label: "Poland",
+  iconName: "PL",
+};
+
 /**
  * Navbar
  *
@@ -71,16 +77,25 @@ export const Navbar = ({
   const [selectedLanguage, setSelectedLanguage] = useState(
     initialLanguage || englishLanguage
   );
-  const [selectedCountry, setSelectedCountry] = useState(kazakhstanCountry);
+
+  const defaultCountry =
+    localStorage.getItem("country") === "KZ"
+      ? kazakhstanCountry
+      : polandCountry;
+
+  const [selectedCountry, setSelectedCountry] = useState(defaultCountry);
+
+  useEffect(() => {
+    if (initialCountry) {
+      setSelectedCountry(initialCountry);
+    }
+  }, [initialCountry]);
 
   useEffect(() => {
     if (initialLanguage) {
       setSelectedLanguage(initialLanguage);
     }
-    if (initialCountry) {
-      setSelectedCountry(initialCountry);
-    }
-  }, [initialLanguage, initialCountry]);
+  }, [initialLanguage]);
 
   useEffect(() => {
     if (selectedLanguage && countries && languages) {
