7b6b2a4fcadafb30832dd995d8ea1c610d6d4fa0
7Digit-Main <it-support@7digit.io>
2025-05-01 19:34:11 +0300

    Fix: redirections for website

--- src/components/navbars/Navbar/Navbar.jsx
+++ src/components/navbars/Navbar/Navbar.jsx
@@ -178,7 +178,7 @@ export const Navbar = ({
   let items = [];
   pages.forEach((page) => {
     const url = `${
-      renderIn === "website" ? "/" : `/${renderIn}`
+      renderIn === "website" ? "" : `/${renderIn}`
     }/${localStorage.getItem("language")}${page.url}`;
 
     items.push({
