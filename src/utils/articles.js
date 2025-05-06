f0b6c9d6422b79ff5f1b92be1ddc4318c516dd2d
7Digit-Main <it-support@7digit.io>
2025-05-06 14:11:45 +0300

    Add: likes and dislikes to destructure functions

--- src/utils/articles.js
+++ src/utils/articles.js
@@ -9,21 +9,17 @@ function destructureArticleData(article) {
   const body = articleData.body;
   const articleLabels = computeArticleLabels(articleData.labels?.data);
   const articleReadingTime = articleData.reading_time;
-  const articleThumbnailImage =
-    article.attributes.image?.data?.attributes?.formats?.thumbnail?.url;
-  const articleImageMedium =
-    article.attributes.image?.data?.attributes?.formats?.medium?.url;
-  const articleImageSmall =
-    article.attributes.image?.data?.attributes?.formats?.small?.url;
+
+  const imageData = article.attributes.image?.data?.attributes;
+  const articleThumbnailImage = imageData?.formats?.thumbnail?.url;
+  const articleImageMedium = imageData?.formats?.medium?.url;
+  const articleImageSmall = imageData?.formats?.small?.url;
+
   const categoryId = articleData.category?.data?.id;
   const categoryName = articleData.category?.data?.attributes?.name;
   const description = articleData.description;
   const ageGroupId = articleData.age_groups.data[0]?.id;
 
-  // const creator =
-  //   articleData.createdBy.data.attributes.firstname +
-  //   " " +
-  //   articleData.createdBy.data.attributes.lastname;
   const author = articleData.author;
 
   return {
@@ -40,8 +36,8 @@ function destructureArticleData(article) {
     categoryName: categoryName,
     description,
     ageGroupId,
-    likes: articleData.likes || 0,
-    dislikes: articleData.dislikes || 0,
+    likes: Number(articleData.likes) || 0,
+    dislikes: Number(articleData.dislikes) || 0,
   };
 }
 
@@ -67,6 +63,8 @@ const destructureVideoData = (videoData) => {
     category,
     labels: labelsData,
     vimeoThumbnailUrl,
+    likes,
+    dislikes,
   } = videoData.attributes;
 
   // Get thumbnail URLs with fallbacks
@@ -120,12 +118,9 @@ const destructureVideoData = (videoData) => {
     labels,
     vimeoThumbnailUrl,
     image,
-    // Add empty values for fields that might be expected based on article structure
-    // but are not present in video data
-    labels: [],
+    likes: Number(likes),
+    dislikes: Number(dislikes),
     creator: null,
-    likes: 0,
-    dislikes: 0,
   };
 };
 
