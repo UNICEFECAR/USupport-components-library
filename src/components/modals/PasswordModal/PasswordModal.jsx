f2424441d44f0cb30110353790a6cbb5fcb93635
7Digit-Main <it-support@7digit.io>
2025-07-04 16:03:54 +0300

    Add: logs

--- src/components/modals/PasswordModal/PasswordModal.jsx
+++ src/components/modals/PasswordModal/PasswordModal.jsx
@@ -22,7 +22,8 @@ export const PasswordModal = ({
   placeholder,
 }) => {
   const [value, setValue] = useState("");
-
+  console.log(process.env.NODE_ENV, "process.env.NODE_ENV");
+  console.log(isOpen, "isOpen");
   if (
     process.env.NODE_ENV === "development" ||
     process.env.NODE_ENV === "production"
