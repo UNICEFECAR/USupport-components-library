c3d477ecb4296148206329b4237ef6ac8bd92703
7Digit-main <it-support@7digit.io>
2025-07-17 17:49:23 +0300

    Refactor: Tabs component styling

--- src/components/tabs/Tabs/Tabs.jsx
+++ src/components/tabs/Tabs/Tabs.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import PropTypes from "prop-types";
+
+import { Icon } from "../../icons";
 
 import "./tabs.scss";
 
@@ -11,11 +13,64 @@ import "./tabs.scss";
  * @return {jsx}
  */
 export const Tabs = ({ options, handleSelect, t = () => {} }) => {
+  const scrollContainerRef = useRef(null);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(false);
+
   const handleOnSelect = (index) => {
     if (handleSelect) {
       handleSelect(index);
     }
   };
+
+  const checkScrollability = () => {
+    if (scrollContainerRef.current) {
+      const { scrollLeft, scrollWidth, clientWidth } =
+        scrollContainerRef.current;
+      const tolerance = 1;
+      setCanScrollLeft(scrollLeft > tolerance);
+      setCanScrollRight(scrollLeft < scrollWidth - clientWidth - tolerance);
+    }
+  };
+
+  const scrollTabs = (direction) => {
+    if (scrollContainerRef.current) {
+      const scrollAmount = scrollContainerRef.current.clientWidth / 2.5;
+      const newScrollLeft =
+        direction === "left"
+          ? scrollContainerRef.current.scrollLeft - scrollAmount
+          : scrollContainerRef.current.scrollLeft + scrollAmount;
+
+      scrollContainerRef.current.scrollTo({
+        left: newScrollLeft,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  useEffect(() => {
+    checkScrollability();
+
+    const handleResize = () => {
+      checkScrollability();
+    };
+
+    const handleScroll = () => {
+      checkScrollability();
+    };
+
+    window.addEventListener("resize", handleResize);
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (scrollContainerRef.current) {
+        scrollContainerRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [options]);
 
   const renderOptions = () => {
     if (!options || !Array.isArray(options)) {
@@ -39,7 +94,27 @@ export const Tabs = ({ options, handleSelect, t = () => {} }) => {
 
   return (
     <div className="tabs-wrapper">
-      <div className="tabs">{renderOptions()}</div>
+      <div className="tabs">
+        <div
+          className={`tab-arrow tab-arrow--left ${
+            !canScrollLeft ? "tab-arrow--disabled" : ""
+          }`}
+          onClick={() => canScrollLeft && scrollTabs("left")}
+        >
+          <Icon name="arrow-chevron-back" />
+        </div>
+        <div className="tabs-container" ref={scrollContainerRef}>
+          {renderOptions()}
+        </div>
+        <div
+          className={`tab-arrow tab-arrow--right ${
+            !canScrollRight ? "tab-arrow--disabled" : ""
+          }`}
+          onClick={() => canScrollRight && scrollTabs("right")}
+        >
+          <Icon name="arrow-chevron-forward" />
+        </div>
+      </div>
     </div>
   );
 };
