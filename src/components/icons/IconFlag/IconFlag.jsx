87642317e625721dd3081926e43f3854aed7252c
7Digit-main <it-support@7digit.io>
2025-08-20 17:19:49 +0300

    Fix: improve color logic for IconFlag component based on theme

--- src/components/icons/IconFlag/IconFlag.jsx
+++ src/components/icons/IconFlag/IconFlag.jsx
@@ -22,9 +22,11 @@ export const IconFlag = ({ flagName }) => {
       size="md"
       aria-label={flagName}
       color={
-        theme === "highContrast" && flagName === "global"
-          ? "#ffff00"
-          : "#20809e"
+        flagName === "global"
+          ? theme === "highContrast"
+            ? "#ffff00"
+            : "#20809e"
+          : undefined
       }
     />
   );
