f88ba1b906995e88f86dabd9ccd1e8724853082a
7Digit-Main <it-support@7digit.io>
2026-02-09 16:04:33 +0200

    Add: email field to sendAdmin request

--- src/services/email.js
+++ src/services/email.js
@@ -8,14 +8,16 @@ const API_ENDPOINT = `${import.meta.env.VITE_API_ENDPOINT}/v1/email`;
  * @param {string} subject text written in the subjet of the email
  * @param {string} title the title of the email
  * @param {string} text the text of the email
+ * @param {string} email email for reply-to
  *
  * @returns {boolean} false if there was a problem with the email
  */
-async function sendAdmin({ subject, title, text }) {
+async function sendAdmin({ subject, title, text, email }) {
   const response = await http.post(`${API_ENDPOINT}/admin`, {
     subject,
     title,
     text,
+    email,
   });
 
   return response;
