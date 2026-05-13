33fe2d862cf1cd4e9eaea58923a76f8b302eeb61
7Digit-main <it-support@7digit.io>
2026-05-13 15:06:56 +0300

    Enhance: update OrganizationDetails component with responsive layout, improved styling, and new report button functionality

--- src/components/blocks/OrganizationDetails/OrganizationDetails.jsx
+++ src/components/blocks/OrganizationDetails/OrganizationDetails.jsx
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { Grid, GridItem } from "../../grids";
 import { Icon } from "../../icons";
-import { Avatar } from "../../avatars";
+import { Box } from "../../boxes";
+import { NewButton } from "../../buttons/Button";
 import "./organization-details.scss";
 
 /**
@@ -18,7 +19,9 @@ export const OrganizationDetails = ({
   t,
   buttonComponent,
   handleCopyLink,
+  onReportClick,
   iconColor = "#66768D",
+  classes,
 }) => {
   const renderSpecialisations = React.useCallback(() => {
     if (organization?.specialisations?.length > 0) {
@@ -66,177 +69,196 @@ export const OrganizationDetails = ({
     return organization?.description || null;
   }, [organization, i18n.language]);
 
+  const handleReportInactive = () => {
+    onReportClick?.();
+  };
+
   return (
-    <Grid classes="organization-details__grid">
-      <GridItem md={4} lg={4}>
-        <div className="organization-details__header">
-          <div className="organization-details__header__organization-container">
-            {/* <Avatar
-              image={image}
-              classes="organization-details__header__organization-container__avatar"
-            /> */}
-            <div className="organization-details__header__organization-container__text-container">
-              <div className="organization-details__header__organization-container__text-container__name-container">
-                <h4 className="organization-details__header__organization-container__text-container__name">
-                  {organization?.name}
-                </h4>
-                <div
-                  onClick={handleCopyLink}
-                  className="organization-details__header__organization-container__text-container__share"
-                >
-                  <Icon name="share" size="sm" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {organization?.phone && (
-          <div className="organization-details__information-container-with-icon">
-            <div>
-              <Icon
-                name="call"
-                size="md"
-                color={iconColor}
-              />
-            </div>
-            <p className="paragraph">{organization.phone}</p>
-          </div>
-        )}
-        {organization?.email && (
-          <div className="organization-details__information-container-with-icon">
-            <div>
-              <Icon
-                name="mail-admin"
-                size="md"
-                color={iconColor}
-              />
-            </div>
-            <p className="paragraph">{organization.email}</p>
-          </div>
-        )}
-        {organization?.websiteUrl && (
-          <div className="organization-details__information-container-with-icon">
-            <div>
-              <Icon
-                name="globe"
-                size="md"
-                color={iconColor}
-              />
-            </div>
-            <p className="paragraph">
-              <a
-                href={organization.websiteUrl}
-                target="_blank"
-                rel="noopener noreferrer"
+    <Box
+      classes={[
+        "organization-details",
+        "organization-details__box",
+        classes,
+      ]
+        .filter(Boolean)
+        .join(" ")}
+      liquidGlass
+    >
+      <Grid classes="organization-details__grid">
+        <GridItem xs={4} md={8} lg={12}>
+          <div className="organization-details__header">
+            <div className="organization-details__header__title-row">
+              <h4 className="organization-details__header__title">
+                {organization?.name}
+              </h4>
+              <div
+                onClick={handleCopyLink}
+                className="organization-details__header__share"
               >
-                {organization.websiteUrl}
-              </a>
-            </p>
+                <Icon name="share" size="sm" />
+              </div>
+            </div>
+            <div className="organization-details__header__report">
+              <NewButton
+                label={t("report_inactive")}
+                type="red"
+                size="md"
+                onClick={handleReportInactive}
+              />
+            </div>
           </div>
-        )}
-      </GridItem>
+        </GridItem>
 
-      <GridItem md={8} lg={8}>
-        <Grid>
-          <GridItem md={4} lg={6} classes="organization-details__grid__item">
-            {organization?.address && (
-              <div className="organization-details__information-container">
-                <p className="paragraph organization-details__information-container__heading">
-                  {t("address_label")}
-                </p>
-                <p className="paragraph organization-details__information-container__text">
-                  {organization.address}
-                </p>
-              </div>
-            )}
+        <GridItem
+          xs={4}
+          md={4}
+          lg={6}
+          classes="organization-details__grid__column"
+        >
+          {(organization?.phone ||
+            organization?.email ||
+            organization?.websiteUrl) && (
+            <div className="organization-details__contacts">
+              {organization?.phone && (
+                <div className="organization-details__information-container-with-icon">
+                  <div>
+                    <Icon name="call" size="md" color={iconColor} />
+                  </div>
+                  <p className="text">{organization.phone}</p>
+                </div>
+              )}
+              {organization?.email && (
+                <div className="organization-details__information-container-with-icon">
+                  <div>
+                    <Icon name="mail-admin" size="md" color={iconColor} />
+                  </div>
+                  <p className="text">{organization.email}</p>
+                </div>
+              )}
+              {organization?.websiteUrl && (
+                <div className="organization-details__information-container-with-icon">
+                  <div>
+                    <Icon name="globe" size="md" color={iconColor} />
+                  </div>
+                  <p className="text">
+                    <a
+                      href={organization.websiteUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {organization.websiteUrl}
+                    </a>
+                  </p>
+                </div>
+              )}
+            </div>
+          )}
+        </GridItem>
 
-            {organization?.district?.name && (
-              <div className="organization-details__information-container">
-                <p className="paragraph organization-details__information-container__heading">
-                  {t("sector_label")}
-                </p>
-                <p className="paragraph organization-details__information-container__text">
-                  {t(organization.district.name)}
-                </p>
-              </div>
-            )}
+        <GridItem
+          xs={4}
+          md={4}
+          lg={6}
+          classes="organization-details__grid__column"
+        >
+          {organization?.address && (
+            <div className="organization-details__information-container">
+              <p className="text organization-details__information-container__heading">
+                {t("address_label")}
+              </p>
+              <p className="text organization-details__information-container__text">
+                {organization.address}
+              </p>
+            </div>
+          )}
 
-            {renderPaymentMethods() && (
-              <div className="organization-details__information-container">
-                <p className="paragraph organization-details__information-container__heading">
-                  {t("payment_methods_label")}
-                </p>
-                <p className="paragraph organization-details__information-container__text">
-                  {renderPaymentMethods()}
-                </p>
-              </div>
-            )}
+          {organization?.district?.name && (
+            <div className="organization-details__information-container">
+              <p className="text organization-details__information-container__heading">
+                {t("sector_label")}
+              </p>
+              <p className="text organization-details__information-container__text">
+                {t(organization.district.name)}
+              </p>
+            </div>
+          )}
 
-            {renderUserInteractions() && (
-              <div className="organization-details__information-container">
-                <p className="paragraph organization-details__information-container__heading">
-                  {t("user_interactions_label")}
-                </p>
-                <p className="paragraph organization-details__information-container__text">
-                  {renderUserInteractions()}
-                </p>
-              </div>
-            )}
+          {renderPaymentMethods() && (
+            <div className="organization-details__information-container">
+              <p className="text organization-details__information-container__heading">
+                {t("payment_methods_label")}
+              </p>
+              <p className="text organization-details__information-container__text">
+                {renderPaymentMethods()}
+              </p>
+            </div>
+          )}
 
-            {renderPropertyTypes() && (
-              <div className="organization-details__information-container">
-                <p className="paragraph organization-details__information-container__heading">
-                  {t("property_types_label")}
-                </p>
-                <p className="paragraph organization-details__information-container__text">
-                  {renderPropertyTypes()}
-                </p>
-              </div>
-            )}
+          {renderUserInteractions() && (
+            <div className="organization-details__information-container">
+              <p className="text organization-details__information-container__heading">
+                {t("user_interactions_label")}
+              </p>
+              <p className="text organization-details__information-container__text">
+                {renderUserInteractions()}
+              </p>
+            </div>
+          )}
 
-            {renderSpecialisations() && (
-              <div className="organization-details__information-container">
-                <p className="paragraph organization-details__information-container__heading">
-                  {t("offered_services_label")}
-                </p>
-                <p className="paragraph organization-details__information-container__text">
-                  {renderSpecialisations()}
-                </p>
-              </div>
-            )}
+          {renderPropertyTypes() && (
+            <div className="organization-details__information-container">
+              <p className="text organization-details__information-container__heading">
+                {t("property_types_label")}
+              </p>
+              <p className="text organization-details__information-container__text">
+                {renderPropertyTypes()}
+              </p>
+            </div>
+          )}
 
-            {organization?.providers?.length > 0 && (
-              <div className="organization-details__information-container">
-                <p className="paragraph organization-details__information-container__heading">
-                  {t("providers_label")}
-                </p>
-                <p className="paragraph organization-details__information-container__text">
-                  {organization.providers
-                    .map((provider) => `${provider.name} ${provider.surname}`)
-                    .join(", ")}
-                </p>
-              </div>
-            )}
+          {renderSpecialisations() && (
+            <div className="organization-details__information-container">
+              <p className="text organization-details__information-container__heading">
+                {t("offered_services_label")}
+              </p>
+              <p className="text organization-details__information-container__text">
+                {renderSpecialisations()}
+              </p>
+            </div>
+          )}
+
+          {organization?.providers?.length > 0 && (
+            <div className="organization-details__information-container">
+              <p className="text organization-details__information-container__heading">
+                {t("providers_label")}
+              </p>
+              <p className="text organization-details__information-container__text">
+                {organization.providers
+                  .map((provider) => `${provider.name} ${provider.surname}`)
+                  .join(", ")}
+              </p>
+            </div>
+          )}
+
+          {description && (
+            <div className="organization-details__information-container">
+              <p className="text organization-details__information-container__heading">
+                {t("other_services_label")}
+              </p>
+              <p className="text organization-details__information-container__text">
+                {description}
+              </p>
+            </div>
+          )}
+        </GridItem>
+
+        {buttonComponent ? (
+          <GridItem xs={4} md={8} lg={12}>
+            {buttonComponent}
           </GridItem>
-
-          <GridItem md={4} lg={6} classes="organization-details__grid__item">
-            {description && (
-              <div className="organization-details__information-container">
-                <p className="paragraph organization-details__information-container__heading">
-                  {t("other_services_label")}
-                </p>
-                <p className="paragraph organization-details__information-container__text">
-                  {description}
-                </p>
-              </div>
-            )}
-          </GridItem>
-        </Grid>
-      </GridItem>
-
-      {buttonComponent}
-    </Grid>
+        ) : null}
+      </Grid>
+    </Box>
   );
 };
 
@@ -262,7 +284,17 @@ OrganizationDetails.propTypes = {
   handleCopyLink: PropTypes.func,
 
   /**
+   * Opens organization report flow (e.g. confirmation modal)
+   */
+  onReportClick: PropTypes.func,
+
+  /**
    * Icon color in HEX format
    */
   iconColor: PropTypes.string,
+
+  /**
+   * Extra classes for the outer Box (e.g. page-specific spacing hooks)
+   */
+  classes: PropTypes.string,
 };
